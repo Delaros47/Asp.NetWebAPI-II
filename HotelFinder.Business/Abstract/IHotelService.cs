919a087753ca3041b7bbcfb8d5c66156a96fcbc0
ahmet hamdi ahmetoglu <Ajdihak_med@hotmail.com>
2022-03-06 09:52:23 +0300

    ...

---
+++ HotelFinder.Business/Abstract/IHotelService.cs
@@ -0,0 +1,18 @@
+﻿using HotelFinder.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelFinder.Business.Abstract
+{
+    public interface IHotelService
+    {
+        List<Hotel> GetHotels();
+        Hotel GetHotelById(int id);
+        Hotel Add(Hotel hotel);
+        Hotel Update(Hotel hotel);
+        void Delete(int id);
+    }
+}
---
+++ HotelFinder.Business/Concrete/HotelManager.cs
@@ -0,0 +1,47 @@
+﻿using HotelFinder.Business.Abstract;
+using HotelFinder.DataAccess.Abstract;
+using HotelFinder.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelFinder.Business.Concrete
+{
+    public class HotelManager : IHotelService
+    {
+
+        private IHotelDal _hotelDal;
+        public HotelManager(IHotelDal hotelDal)
+        {
+            _hotelDal = hotelDal;
+        }
+
+        public Hotel Add(Hotel hotel)
+        {
+           return _hotelDal.Add(hotel);
+            
+        }
+
+        public void Delete(int id)
+        {
+            _hotelDal.Delete(id);
+        }
+
+        public Hotel GetHotelById(int id)
+        {
+           return _hotelDal.GetHotelById(id);          
+        }
+
+        public List<Hotel> GetHotels()
+        {
+            return _hotelDal.GetHotels();
+        }
+
+        public Hotel Update(Hotel hotel)
+        {
+            return _hotelDal.Update(hotel);
+        }
+    }
+}
